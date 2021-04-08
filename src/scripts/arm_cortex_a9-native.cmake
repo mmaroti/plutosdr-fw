59f97518b1fe6a82a9463b87894d4fa81ce63bb9
Miklos Maroti <mmaroti@gmail.com>
2021-04-09 01:24:16 +0200

    better cmake toolchain file

--- src/scripts/arm_cortex_a9-native.cmake
+++ src/scripts/arm_cortex_a9-native.cmake
@@ -1,5 +1,5 @@
 ########################################################################
-# Toolchain file for building native on an ARM Cortex A7 w/ NEON
+# Toolchain file for building native on an Zynq ARM Cortex A9 w/ NEON
 # Usage: cmake -DCMAKE_TOOLCHAIN_FILE=<this file> <source directory>
 ########################################################################
 
@@ -10,5 +10,5 @@ set(CMAKE_CXX_COMPILER arm-linux-gnueabihf-g++)
 set(CMAKE_C_COMPILER arm-linux-gnueabihf-gcc)
 set(CMAKE_FIND_ROOT_PATH ${CMAKE_CURRENT_LIST_DIR}/../../buildroot/output/host)
 
-set(CMAKE_CXX_FLAGS "-march=armv7-a -mtune=cortex-a7 -mfpu=neon-vfpv4 -mfloat-abi=hard -ffast-math -Wno-psabi" CACHE STRING "")
+set(CMAKE_CXX_FLAGS "-mcpu=cortex-a9 -mtune=cortex-a9 -mfpu=neon-fp16 -mfloat-abi=hard -ffast-math -Wno-psabi" CACHE STRING "")
 set(CMAKE_C_FLAGS "${CMAKE_CXX_FLAGS}" CACHE STRING "")
